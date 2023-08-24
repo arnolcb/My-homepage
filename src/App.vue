82ea9ece1d42a305c04594b8ac2b260af283486c
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-08-24 12:11:48 -0500

    feat: install analytics

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import { Analytics } from '@vercel/analytics/react';
 </script>
 
 <template>
   <RouterView />
+  <Analytics />
 </template>
